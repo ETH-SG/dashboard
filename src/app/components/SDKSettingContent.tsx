205fd8b3f089388dd86bab3b136c8b3f0814418a
makluganteng <vincentowent1310@gmail.com>
2024-09-22 00:54:56 +0800

    added sniptes

--- src/app/components/SDKSettingContent.tsx
+++ src/app/components/SDKSettingContent.tsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import { CodeBlock } from 'react-code-block';
+
+const codeExample = `
+import {
+  ReversifiWidget,
+  useReversifiContext,
+  getAllEscrow,
+} from "reversifi-sdk";
+
+....
+
+const primaryWallet = useReversifiContext();
+
+const handleClick = () => {
+    // Run the first command (call the getAllEscrow function)
+    getAllEscrow(primaryWallet.primaryWallet);
+    
+   ....
+  };
+
+  return (
+  <div>
+  .....
+  <ReversifiWidget />
+  .....
+  </div>
+  )
+`
 
 const SDKSettingsContent = () => (
   <div>
     <h2 className="text-2xl font-bold mb-4">SDK Settings and Snippets</h2>
     <h1> Snippets</h1>
+    <CodeBlock code={codeExample} language="js">
+      <CodeBlock.Code className="bg-gray-900 p-6 rounded-xl shadow-lg">
+        <CodeBlock.LineContent>
+          <CodeBlock.Token />
+        </CodeBlock.LineContent>
+      </CodeBlock.Code>
+    </CodeBlock>
   </div>
 );
 
