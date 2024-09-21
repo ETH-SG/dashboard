fd95517baebe6f4324d8347acde23555275f318a
0xGavinOwen <gavinowen713@gmail.com>
2024-09-22 02:02:19 +0800

    feat: force push main

--- src/app/MainPage/page.tsx
+++ src/app/MainPage/page.tsx
@@ -1,9 +1,19 @@
 "use client";
-import React from "react";
-
+import React, { useEffect } from "react";
 import { Preview } from "./components/Preview";
+import { useAccount } from "wagmi";
+import { useRouter } from "next/navigation";
 
 const MainPage = () => {
+  const router = useRouter();
+  const { address } = useAccount();
+
+  useEffect(() => {
+    if (!address) {
+      router.push("/");
+    }
+  }, [address]);
+
   return (
     <div>
       <br />
