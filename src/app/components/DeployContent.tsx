0b09c06a4c3d94394642db57e7939e5ecb936b67
0xGavinOwen <gavinowen713@gmail.com>
2024-09-21 20:19:13 +0800

    feat: add get deploy contract

--- src/app/components/DeployContent.tsx
+++ src/app/components/DeployContent.tsx
@@ -19,14 +19,43 @@ type ParsedEscrowCreatedLog = Log & {
   };
 };
 
+type ApiResponse = {
+  data: {
+    id: number;
+    organization_wallet: string;
+    escrow_factory: string;
+    created_at: string;
+  }[];
+};
+
 const DeployContent = () => {
   const { address } = useAccount();
   const [account, setAccount] = useState<`0x${string}` | undefined>(undefined);
+
+  const [apiResponse, setApiResponse] = useState<ApiResponse | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     if (address) {
       setAccount(address);
+      fetchFactoryEscrow(address);
     }
   }, [address]);
+
+  const fetchFactoryEscrow = async (address: string) => {
+    setIsLoading(true);
+    try {
+      const response = await axios.get<ApiResponse>(
+        `http://34.84.200.57:8000/api/escrow/getFactoryEscrow/${address}`
+      );
+      setApiResponse(response.data);
+    } catch (error) {
+      console.error("Error fetching factory escrow:", error);
+      setMessage("Error fetching factory escrow data");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   const { data: hash, error, isPending, writeContract } = useWriteContract();
   const [message, setMessage] = useState("");
@@ -43,7 +72,27 @@ const DeployContent = () => {
   // New state for created Escrow address
   const [createdEscrowAddress, setCreatedEscrowAddress] = useState("");
 
-  // Watch for EscrowCreated event
+  useEffect(() => {
+    if (isConfirmed && createdEscrowAddress) {
+      console.log("New Factory contract created at:", createdEscrowAddress);
+      // You can perform additional actions here with the new contract address
+    }
+  }, [isConfirmed, createdEscrowAddress]);
+
+  const handleDeploy = () => {
+    try {
+      writeContract({
+        abi: newAbi,
+        address: "0xb38e4d2bee4Ea3cb7EcFf3a0dF8aFc9Fafca023D",
+        functionName: "createEscrow",
+        args: [name, address1, address2],
+      });
+    } catch (error) {
+      console.error("Error deploying contract", error);
+      setMessage("Error deploying contract");
+    }
+  };
+
   // Watch for EscrowCreated event
   useWatchContractEvent({
     address: "0xb38e4d2bee4Ea3cb7EcFf3a0dF8aFc9Fafca023D",
@@ -83,27 +132,6 @@ const DeployContent = () => {
     }
   };
 
-  useEffect(() => {
-    if (isConfirmed && createdEscrowAddress) {
-      console.log("New Factory contract created at:", createdEscrowAddress);
-      // You can perform additional actions here with the new contract address
-    }
-  }, [isConfirmed, createdEscrowAddress]);
-
-  const handleDeploy = () => {
-    try {
-      writeContract({
-        abi: newAbi,
-        address: "0xb38e4d2bee4Ea3cb7EcFf3a0dF8aFc9Fafca023D",
-        functionName: "createEscrow",
-        args: [name, address1, address2],
-      });
-    } catch (error) {
-      console.error("Error deploying contract", error);
-      setMessage("Error deploying contract");
-    }
-  };
-
   return (
     <div>
       <h2 className="text-3xl font-black mb-4">Deploy</h2>
@@ -112,37 +140,52 @@ const DeployContent = () => {
 
       <br />
       <br />
-      <h1 className="font-bold">Deploy Contract with parameters</h1>
-      <div>
-        <input
-          type="text"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="border p-2 mb-2 w-full"
-        />
-        <input
-          type="text"
-          placeholder="Address 1"
-          value={address1}
-          onChange={(e) => setAddress1(e.target.value)}
-          className="border p-2 mb-2 w-full"
-        />
-        <input
-          type="text"
-          placeholder="Address 2"
-          value={address2}
-          onChange={(e) => setAddress2(e.target.value)}
-          className="border p-2 mb-2 w-full"
-        />
-        <button
-          onClick={handleDeploy}
-          disabled={isPending || !name || !address1 || !address2}
-          className="bg-blue-500 text-white p-2 rounded disabled:bg-gray-300"
-        >
-          {isPending ? "Deploying..." : "Deploy Contract"}
-        </button>
-      </div>
+
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : apiResponse && apiResponse.data.length > 0 ? (
+        <div>
+          <h1 className="font-bold">Existing Escrow Factory Contract</h1>
+          <p>
+            Escrow factory contract is: {apiResponse.data[0].escrow_factory}
+          </p>
+        </div>
+      ) : (
+        <div>
+          <h1 className="font-bold">Deploy Contract with parameters</h1>
+          <div>
+            <input
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="border p-2 mb-2 w-full"
+            />
+            <input
+              type="text"
+              placeholder="Address 1"
+              value={address1}
+              onChange={(e) => setAddress1(e.target.value)}
+              className="border p-2 mb-2 w-full"
+            />
+            <input
+              type="text"
+              placeholder="Address 2"
+              value={address2}
+              onChange={(e) => setAddress2(e.target.value)}
+              className="border p-2 mb-2 w-full"
+            />
+            <button
+              onClick={handleDeploy}
+              disabled={isPending || !name || !address1 || !address2}
+              className="bg-blue-500 text-white p-2 rounded disabled:bg-gray-300"
+            >
+              {isPending ? "Deploying..." : "Deploy Contract"}
+            </button>
+          </div>
+        </div>
+      )}
+
       {(hash ||
         isConfirming ||
         isConfirmed ||
